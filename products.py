ed96fd31318d405e00620637e68a86ae998dab29
Christopher L. Lübbers <wsad@posteo.net>
2026-02-09 13:42:42 +0100

    create Product class

---
+++ products.py
@@ -0,0 +1,60 @@
+class Product:
+    def __init__(self, name, price, quantity):
+        if not name:
+            raise ValueError("Enter a name")
+        else: 
+            self.name = name
+        
+        if price < 0 or price is type(str):
+            raise ValueError("Price should be a positive number")
+        else:
+            self.price = price
+        
+        if quantity < 0:
+            raise ValueError("Quantity has to be positive!")
+        else:
+            self.quantity = quantity
+            
+        self.active = True
+    
+    def get_quantity(self) -> int:
+        return self.quantity
+    
+    def set_quantity(self, quantity):
+        self.quantity = quantity
+        if quantity == 0:
+            self.active = False
+    
+    def is_active(self) -> bool:
+        return self.active
+    
+    def activate(self):
+        self.active = True
+        
+    def deactivate(self):
+        self.activate = False
+        
+    def show(self):
+        print(f"{self.name}, Price: {self.price}, Quantity: {self.quantity}")
+        
+    def buy(self, quantity) -> float:
+        if quantity > self.quantity:
+            raise ValueError(f"There are only {self.quantity} left!")
+        else: 
+            self.quantity = self.quantity - quantity
+        return self.price * quantity
+    
+    
+    
+bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
+mac = Product("MacBook Air M2", price=1450, quantity=100)
+
+print(bose.buy(50))
+print(mac.buy(100))
+print(mac.is_active())
+
+bose.show()
+mac.show()
+
+bose.set_quantity(1000)
+bose.show()
